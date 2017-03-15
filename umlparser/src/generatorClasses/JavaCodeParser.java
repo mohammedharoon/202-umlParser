69ad190c18a38cfd89f32a38a58770c9df3e5723
Mohammed Haroon Shareef <aqyar_shareef_77@yahoo.com>
2017-03-15 00:52:51 -0700

    converted the project to maven to support javaparser

--- umlparser/src/generatorClasses/JavaCodeParser.java
+++ umlparser/src/generatorClasses/JavaCodeParser.java
@@ -1,5 +1,25 @@
 package generatorClasses;
 
+import java.io.File;
+import java.io.FileInputStream;
+
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.ast.*;
+import com.github.javaparser.ast.body.*;
+import com.github.javaparser.ast.type.ClassOrInterfaceType;
+
+
 public class JavaCodeParser {
 
+	public static void main(String args[]) throws Exception
+	{
+		CompilationUnit compilationUnit = JavaParser.parse("class A { }");
+		//ClassOrInterfaceDeclaration classA = compilationUnit.getClassByName("A");
+		String basePath = new File("").getAbsolutePath();
+		FileInputStream in = new FileInputStream(basePath+"/GenerateClassDiagram.java");
+
+        // parse the file
+        CompilationUnit cu = JavaParser.parse(in);
+        System.out.println(cu.toString());
+	}
 }
